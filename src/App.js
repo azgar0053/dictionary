0e86cd305ccdd1ad5877c4db8e104a629d2da299
azgar0053 <azgar53ali@gmail.com>
2024-01-08 12:35:20 +0530

    updated basic dictionary app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import {useState } from 'react';
+const words = [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+];
+
+
+function App() {
+  const [input, setInput] = useState('');
+  const [meaning, setMeaning] = useState('')
+
+  const handleChange=(e)=>{
+    setInput(e.target.value)
+    if(e.target.value===""){
+      setMeaning('')
+    }
+  }
+
+const handleSearch=()=>{
+  const foundWord = words.find((w)=>w.word.toLowerCase()==input.toLowerCase());
+
+
+  if(foundWord){
+    setMeaning(foundWord.meaning)
+  }else{
+  setMeaning("Word not found in the dictionary.")}
+}
+
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <input type='text' value={input} onChange={handleChange} required/>
+      <button onClick={handleSearch}>Search</button>
+      <p><strong>Definition:</strong> {meaning}</p>
+    </div>
+  );
+}
+
+export default App;
